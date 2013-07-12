33e09907901eec337f37235e2f8a270b201c0f45
panavia <panavia@Undef8560U.(none)>
2013-07-12 14:33:46 -0700

    initial combination of files

---
+++ knotilus_classes.py
@@ -0,0 +1,42 @@
+# SURF 2013
+# FILE: knotilus_classes.py
+# AUTHOR: Laura Shou
+# MENTOR: Professor Yi Ni
+# 06.21.13
+
+
+'''
+This module defines the KEdgeClass, which is very similar to the EdgeClass 
+from plink_load.
+
+VertexClass, IntersectionClass, RegionClass, and VectorClass are imported from
+plink_classes.py.
+
+VertexClass, KEdgeClass, IntersectionClass, and RegionClass objects will be
+stored in global variables gVertices, gEdges, gIntersections, and gKRegions, 
+respectively.
+'''
+
+from plink_classes import VectorClass, IntersectionClass, VertexClass, \
+     RegionClass
+
+class KEdgeClass():
+    ''' 
+    Informally, an KEdge object consists of 2 segments. 
+    The two segments are (flexpt1 - intersection) and (intersection - flexpt2).
+    The segments are not important however; the necessary information is the
+    two flex points and the intersection on the KEdge.
+    '''
+    def __init__(self, index, point1, point2, points, intersection):
+        self.index = index # index in gKEdges
+        self.point1 = point1 # flex point (VertexClass)
+        self.point2 = point2 # flex point (VertexClass)
+        self.points = points # indicies for flex points (index1, index2)
+        self.inter = intersection # IntersectionClass
+        self.vertices = [point1, point2] 
+
+    def draw(self, canvas):
+        '''Draw it in Tkinter, on canvas 'canvas'.'''
+        return canvas.create_line(self.point1.x, self.point1.y, \
+                                   self.inter.x, self.inter.y, \
+                                   self.point2.x, self.point2.y)
